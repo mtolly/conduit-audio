8fb91a595cf460e86bb8ad67400c440c2b9211d0
Michael Tolly <miketolly@gmail.com>
2016-01-07 22:45:45 -0600

    Ability to manually setup sndfile and lame

--- conduit-audio-lame/src/Data/Conduit/Audio/LAME.hs
+++ conduit-audio-lame/src/Data/Conduit/Audio/LAME.hs
@@ -14,19 +14,26 @@ import qualified Data.Vector.Storable as V
 import qualified Data.ByteString as B
 import Foreign
 import qualified System.IO as IO
+import Control.Monad.Trans.Class (lift)
 
--- | Saves an audio stream to an MP3 file.
+-- | Saves an audio stream to an MP3 file. Uses the default VBR mode.
 sinkMP3 :: (MonadResource m) => FilePath -> A.AudioSource m Float -> m ()
-sinkMP3 fp (A.AudioSource s r c _) = (C.$$) s
+sinkMP3 fp = sinkMP3WithHandle fp $ \lame -> liftIO $ L.check $ L.setVBR lame L.VbrDefault
+
+-- | Lets you perform arbitrary setup on the `lame` handle before encoding.
+-- You must select a quality somewhere in the setup action,
+-- such as with 'L.setVBR'.
+sinkMP3WithHandle :: (MonadResource m) => FilePath -> (L.LAME -> m ()) -> A.AudioSource m Float -> m ()
+sinkMP3WithHandle fp setup (A.AudioSource s r c _) = (C.$$) s
   $ C.bracketP L.init (L.check . L.close)
   $ \lame -> C.bracketP (IO.openBinaryFile fp IO.WriteMode) IO.hClose
     $ \fout -> do
       let o = liftIO . L.check
       o $ L.setInSamplerate lame $ round r
-      unless (c `elem ` [1, 2]) $ error $
-        "sinkMP3: only 1 or 2 channels are supported (" ++ show c ++ " given)"
+      unless (c `elem ` [1, 2]) $ err $
+        "only 1 or 2 channels are supported (" ++ show c ++ " given)"
       o $ L.setNumChannels lame c
-      o $ L.setVBR lame L.VbrDefault
+      lift $ setup lame
       o $ L.initParams lame
       fix $ \loop -> C.await >>= \mx -> case mx of
         Nothing -> liftIO $ do
@@ -37,7 +44,7 @@ sinkMP3 fp (A.AudioSource s r c _) = (C.$$) s
           IO.hSeek fout IO.AbsoluteSeek 0
           tags <- allocaArray 100000 $ \buf -> do
             len <- L.getLametagFrame lame (castPtr buf) 100000
-            when (len < 0) $ error "sinkMP3: couldn't get lame tag frame (buffer too small)"
+            when (len < 0) $ err "couldn't get lame tag frame (buffer too small)"
             B.packCStringLen (buf, fromIntegral len)
           B.hPutStr fout tags
         Just v -> do
@@ -48,8 +55,8 @@ sinkMP3 fp (A.AudioSource s r c _) = (C.$$) s
               len <- case c of
                 1 -> L.encodeBufferIeeeFloat lame (castPtr p) nullPtr nsamples (castPtr buf) mp3bufsize
                 _ -> L.encodeBufferInterleavedIeeeFloat lame (castPtr p) nsamples (castPtr buf) mp3bufsize
-              when (len < 0) $ error $
-                "sinkMP3: encode function returned " ++ show len ++ "; " ++ case len of
+              when (len < 0) $ err $
+                "encode function returned " ++ show len ++ "; " ++ case len of
                   -1 -> "mp3buf was too small"
                   -2 -> "malloc() problem"
                   -3 -> "lame_init_params() not called"
@@ -58,3 +65,4 @@ sinkMP3 fp (A.AudioSource s r c _) = (C.$$) s
               B.packCStringLen (buf, len)
             B.hPutStr fout bs
           loop
+  where err = error . ("Conduit.Audio.LAME.sinkMP3WithHandle: " ++)
--- conduit-audio-sndfile/src/Data/Conduit/Audio/Sndfile.hs
+++ conduit-audio-sndfile/src/Data/Conduit/Audio/Sndfile.hs
@@ -1,6 +1,6 @@
 module Data.Conduit.Audio.Sndfile
-( sourceSnd, sourceSndFrom
-, sinkSnd
+( sourceSnd, sourceSndFrom, sourceSndWithHandle
+, sinkSnd, sinkSndWithHandle
 ) where
 
 import Data.Conduit.Audio
@@ -9,6 +9,7 @@ import qualified Data.Conduit.List as CL
 import qualified Sound.File.Sndfile as Snd
 import qualified Sound.File.Sndfile.Buffer.Vector as SndBuf
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Class (lift)
 import Control.Monad (void)
 import Control.Monad.Fix (fix)
 import Control.Monad.Trans.Resource (MonadResource)
@@ -20,6 +21,7 @@ sourceSnd
   -> IO (AudioSource m a)
 sourceSnd = sourceSndFrom $ Frames 0
 
+-- | Lets you specify a position to start from in the file.
 sourceSndFrom
   :: (MonadResource m, Snd.Sample a)
   => Duration
@@ -30,6 +32,17 @@ sourceSndFrom (Seconds secs) fp = do
   info <- Snd.getFileInfo fp
   sourceSndFrom (Frames $ secondsToFrames secs $ fromIntegral $ Snd.samplerate info) fp
 sourceSndFrom (Frames fms) fp = do
+  src <- sourceSndWithHandle fp $ \h -> liftIO $ void $ Snd.hSeek h Snd.AbsoluteSeek fms
+  return src{ frames = frames src - fms }
+
+-- | Lets you perform arbitrary setup on the @libsndfile@ handle before decoding.
+sourceSndWithHandle
+  :: (MonadResource m, Snd.Sample a)
+  => FilePath
+  -> (Snd.Handle -> m ())
+  -- ^ Perform any setup necessary with the @libsndfile@ handle
+  -> IO (AudioSource m a)
+sourceSndWithHandle fp setup = do
   -- TODO: allow user to supply Snd.Format for raw files?
   info <- Snd.getFileInfo fp
   let r = fromIntegral $ Snd.samplerate info
@@ -38,21 +51,34 @@ sourceSndFrom (Frames fms) fp = do
         (Snd.openFile fp Snd.ReadMode Snd.defaultInfo)
         Snd.hClose
         $ \h -> do
-          liftIO $ void $ Snd.hSeek h Snd.AbsoluteSeek fms
+          lift $ setup h
           fix $ \loop -> liftIO (Snd.hGetBuffer h chunkSize) >>= \mx -> case mx of
             Nothing  -> return ()
             Just buf -> do
               C.yield $ SndBuf.fromBuffer buf
               loop
-  return $ AudioSource src r c $ Snd.frames info - fms
+  return $ AudioSource src r c $ Snd.frames info
 
 -- | Uses @libsndfile@ to write an audio stream to a file.
 sinkSnd :: (MonadResource m, Snd.Sample a) => FilePath -> Snd.Format -> AudioSource m a -> m ()
-sinkSnd fp fmt (AudioSource s r c _) = s C.$$ C.bracketP
-  (Snd.openFile fp Snd.WriteMode $ Snd.defaultInfo
-    { Snd.format     = fmt
-    , Snd.samplerate = round r
-    , Snd.channels   = c
-    })
-  Snd.hClose
-  (\h -> CL.mapM_ $ liftIO . void . Snd.hPutBuffer h . SndBuf.toBuffer)
+sinkSnd fp fmt = sinkSndWithHandle fp fmt $ \_ -> return ()
+
+-- | Lets you perform arbitrary setup on the @libsndfile@ handle before encoding.
+sinkSndWithHandle
+  :: (MonadResource m, Snd.Sample a)
+  => FilePath
+  -> Snd.Format
+  -> (Snd.Handle -> m ())
+  -> AudioSource m a
+  -> m ()
+sinkSndWithHandle fp fmt setup (AudioSource s r c _) = s C.$$ do
+  C.bracketP
+    (Snd.openFile fp Snd.WriteMode $ Snd.defaultInfo
+      { Snd.format     = fmt
+      , Snd.samplerate = round r
+      , Snd.channels   = c
+      })
+    Snd.hClose
+    $ \h -> do
+      lift $ setup h
+      CL.mapM_ $ liftIO . void . Snd.hPutBuffer h . SndBuf.toBuffer
