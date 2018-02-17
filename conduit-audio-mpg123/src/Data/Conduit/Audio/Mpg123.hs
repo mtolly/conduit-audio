697a92ce61ed548a64e033d048817bbc35caa987
Michael Tolly <miketolly@gmail.com>
2018-02-16 20:15:02 -0600

    Support most mpg123 sample formats

--- conduit-audio-mpg123/src/Data/Conduit/Audio/Mpg123.hs
+++ conduit-audio-mpg123/src/Data/Conduit/Audio/Mpg123.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Conduit.Audio.Mpg123 where
 
 import qualified Codec.Mpg123.Raw             as M
@@ -6,19 +7,43 @@ import           Control.Monad                (unless)
 import           Control.Monad.Fix            (fix)
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Resource (MonadResource)
-import           Data.Bits                    (shiftR)
 import qualified Data.Conduit                 as C
 import           Data.Conduit.Audio
+import           Data.Int
 import qualified Data.Vector.Storable         as V
-import           Foreign                      (alloca, castPtr, copyBytes,
-                                               finalizerFree, free, mallocBytes,
-                                               newForeignPtr, nullPtr, peek)
+import           Data.Word
+import           Foreign
 import           Foreign.C                    (peekCString, withCString)
 
+class (Storable a) => MpgFormat a where
+  mpgFormat :: a -> M.Mpg123_enc_enum
+
+instance MpgFormat Float where
+  mpgFormat _ = M.mpg123_enc_float_32
+
+instance MpgFormat Int8 where
+  mpgFormat _ = M.mpg123_enc_signed_8
+
+instance MpgFormat Int16 where
+  mpgFormat _ = M.mpg123_enc_signed_16
+
+instance MpgFormat Int32 where
+  mpgFormat _ = M.mpg123_enc_signed_32
+
+instance MpgFormat Word8 where
+  mpgFormat _ = M.mpg123_enc_unsigned_8
+
+instance MpgFormat Word16 where
+  mpgFormat _ = M.mpg123_enc_unsigned_16
+
+instance MpgFormat Word32 where
+  mpgFormat _ = M.mpg123_enc_unsigned_32
+
 sourceMpg
-  :: (MonadResource m)
+  :: forall m a
+  .  (MonadResource m, MpgFormat a)
   => FilePath
-  -> IO (AudioSource m Float)
+  -> IO (AudioSource m a)
 sourceMpg fin = do
   let check ctx f = f >>= \n -> if M.Mpg123_errors n == M.mpg123_ok
         then return ()
@@ -27,6 +52,8 @@ sourceMpg fin = do
         x <- f p
         check ctx $ peek p
         return x
+      fmt = mpgFormat (undefined :: a)
+      size = sizeOf (undefined :: a)
   check "mpg123_init" M.c_mpg123_init
   (r, chans, _enc, len) <- bracket (checkp "" $ M.c_mpg123_new nullPtr) M.c_mpg123_delete $ \mh -> do
     bracket_ (check "" $ withCString fin $ M.c_mpg123_open mh) (check "" $ M.c_mpg123_close mh) $ do
@@ -41,8 +68,7 @@ sourceMpg fin = do
         C.bracketP (check "" $ withCString fin $ M.c_mpg123_open mh) (\() -> check "" $ M.c_mpg123_close mh) $ \() -> do
           bufSize <- liftIO $ do
             check "mpg123_format_none" $ M.c_mpg123_format_none mh
-            check "mpg123_format" $ M.c_mpg123_format mh r chans
-              $ M.mpg123_enc_enum M.mpg123_enc_float_32
+            check "mpg123_format" $ M.c_mpg123_format mh r chans $ M.mpg123_enc_enum fmt
             M.c_mpg123_outblock mh
           liftIO $ check "mpg123_getformat" $ M.c_mpg123_getformat mh nullPtr nullPtr nullPtr
           C.bracketP (mallocBytes $ fromIntegral bufSize) free $ \buf -> do
@@ -57,6 +83,6 @@ sourceMpg fin = do
                   buf' <- mallocBytes done
                   copyBytes buf' buf done
                   newForeignPtr finalizerFree $ castPtr buf'
-                C.yield $ V.unsafeFromForeignPtr0 fptr $ done `shiftR` 2
+                C.yield $ V.unsafeFromForeignPtr0 fptr $ done `quot` size
                 loop
   return $ AudioSource src (realToFrac r) (fromIntegral chans) (fromIntegral len)
